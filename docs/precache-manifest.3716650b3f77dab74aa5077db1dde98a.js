bbc48888abdf2b6d029d3fcddf42f75cd0a32211
Alicja Łuczyńska <alicja.luczynska@gmail.com>
2018-10-26 13:27:56 +0200

    add files

---
+++ docs/precache-manifest.3716650b3f77dab74aa5077db1dde98a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "a9f7b9b72bad5b26995a",
+    "url": "./static/js/main.a9f7b9b7.chunk.js"
+  },
+  {
+    "revision": "43180b573286f60eb32f",
+    "url": "./static/js/1.43180b57.chunk.js"
+  },
+  {
+    "revision": "a9f7b9b72bad5b26995a",
+    "url": "./static/css/main.7a3ca738.chunk.css"
+  },
+  {
+    "revision": "4f4f82bab63e8be5dc2a3bc4e54e90ce",
+    "url": "./index.html"
+  }
+];
